fd24bdf601b92c394db819fe8fbdb8d6943fbee1
Alon Avisar <1github@proton.me>
2022-11-09 18:41:41 +0200

    api song details + lyrics

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,17 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-// const options = {
-//     method: 'GET',
-//     headers: {
-//       'X-RapidAPI-Key': '7b60e0cf74msh6f0debccfc8c6fep1baa18jsn419d9e751e33',
-//       'X-RapidAPI-Host': 'shazam-core.p.rapidapi.com'
-//     }
-//   };
-
-//   fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
-//     .then(response => response.json())
-//     .then(response => console.log(response))
-//     .catch(err => console.error(err));
 
 export const shazamCoreApi = createApi({
   reducerPath: "shazamCoreApi",
@@ -27,11 +15,12 @@ export const shazamCoreApi = createApi({
   }),
 
   endpoints: (builder) => ({
-    getTopCharts: builder.query({ query: () => '/charts/world'}),
+    getTopCharts: builder.query({ query: () => "/charts/world" }),
+    getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}`}),
+    getSongDetails: builder.query({query: ({ songid }) => `/tracks/details?track_id=${songid}`,
+    
+    }),
   }),
 });
 
-
-export const {
-    useGetTopChartsQuery,
-} = shazamCoreApi
+export const { useGetTopChartsQuery,  useGetSongDetailsQuery , useGetSongRelatedQuery} = shazamCoreApi;
