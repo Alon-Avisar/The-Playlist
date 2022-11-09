638d19d0aec19713232eedce31b73a03cce587b0
Alon Avisar <1github@proton.me>
2022-11-09 20:35:46 +0200

    adding the search bar

--- src/components/RelatedSongs.jsx
+++ src/components/RelatedSongs.jsx
@@ -1,4 +1,4 @@
-import { SongBar } from "./SongBar";
+import SongBar from "./SongBar";
 
 const RelatedSongs = ({
   data,
@@ -6,14 +6,26 @@ const RelatedSongs = ({
   activeSong,
   handlePauseClick,
   handlePlayClick,
-}) => 
-(
-<div className="flex flex-col">
-  <h1 className="font-bold text-3xl text-white">Related Songs</h1>
-
-</div>
-
-)
-
+  artistId,
+}) => (
+  
+  <div className="flex flex-col">
+    <h1 className="font-bold text-3xl text-white">Related Songs:</h1>
+    <div className="mt-6 w-full flex flex-col">
+      {data?.map((song, i) => (
+        <SongBar
+         key={`${song.key}-${artistId}`}
+         song={song}
+         i={i}
+         artistId={artistId}
+         isPlaying={isPlaying}
+         activeSong={activeSong}
+         handlePauseClick={handlePauseClick}
+         handlePlayClick={handlePlayClick}
+         />
+      ))}
+    </div>
+  </div>
+);
 
 export default RelatedSongs;
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -17,10 +17,12 @@ export const shazamCoreApi = createApi({
   endpoints: (builder) => ({
     getTopCharts: builder.query({ query: () => "/charts/world" }),
     getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}`}),
+    getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}` }),
+    getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}` }),
     getSongDetails: builder.query({query: ({ songid }) => `/tracks/details?track_id=${songid}`,
-    
+
     }),
   }),
 });
 
-export const { useGetTopChartsQuery,  useGetSongDetailsQuery , useGetSongRelatedQuery} = shazamCoreApi;
+export const {  useGetTopChartsQuery,  useGetSongDetailsQuery , useGetSongRelatedQuery , useGetArtistDetailsQuery , useGetSongsByCountryQuery , } = shazamCoreApi;
